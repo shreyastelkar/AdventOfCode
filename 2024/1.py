3df5ccbc7e42cb466da41e4bb402ed03644eeafe
Shreyas Telkar <shretel@gmail.com>
2024-12-12 09:43:38 -0800

    Move files for problem 1y

--- 2024/1.py
+++ 2024/1.py
@@ -1,4 +1,13 @@
-from utils import read_file
+def read_file_and_build_cols(filename: str):
+    first = []
+    second = []
+    with open(filename) as f:
+        for line in f:
+            line_numbers = line.split()
+            first.append(int(line_numbers[0]))
+            second.append(int(line_numbers[1]))
+
+    return first, second
 
 def calculate_total(values):
     total = 0
@@ -7,10 +16,25 @@ def calculate_total(values):
     return total
         
 def calculate_total_distance(filename: str):
-    first, second = read_file(filename)
+    first, second = read_file_and_build_cols(filename)
     zipped_values = list(zip(sorted(first), sorted(second)))
     total = calculate_total(zipped_values)
     return total
 
+def calculate_similarity_score(filename: str):
+    first, second = read_file_and_build_cols(filename)
+
+    #Frequency of second list
+    d = {}
+    for num in second:
+        d[num] = d.get(num, 0) + 1
+
+    score = 0
+    for num in first:
+        # Calculate the score for each value in `first`
+        score += num * d.get(num, 0)
+    return score
+
 if __name__=="__main__":
     print(calculate_total_distance("input.txt"))
+    print(calculate_similarity_score("input.txt"))
