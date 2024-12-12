551d5a5c080f719e6ecc4b1baf93431e57a9da21
Shreyas Telkar <shretel@gmail.com>
2024-12-12 12:48:27 -0800

    Update code

--- 2024/2.py
+++ 2024/2.py
@@ -12,23 +12,21 @@ def verify_report_safety(filename: str):
     
     for report in reports:
         is_safe = True
-        increasing_map = {}
+        is_increasing = None
 
         for level_index in range(1, len(report)):
             diff = report[level_index] - report[level_index - 1]
             
             if 1 <= abs(diff) <= 3:
-                # Check if the previous state was increasing
-                is_increasing = increasing_map.get(level_index - 1)
-                
                 if is_increasing is None:
                     # At the first iteration
-                    increasing_map[level_index] = diff > 0
+                    is_increasing = diff > 0
                 elif (is_increasing and diff < 0) or (not is_increasing and diff > 0):
                     is_safe = False
                     break
                 else:
-                    increasing_map[level_index] = is_increasing
+                    # Leave the increasing trend as is
+                    pass
 
             else:
                 # Duplicate value or out of bounds
