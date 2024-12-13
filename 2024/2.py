4af7e1ac86453bbffe1c031ca079f3a72a9972c9
Shreyas Telkar <shretel@gmail.com>
2024-12-12 20:11:45 -0800

    Update increasing code

--- 2024/2.py
+++ 2024/2.py
@@ -36,5 +36,62 @@ def verify_report_safety(filename: str):
     return safety_counter
 
 
+def verify_report_safety_with_exception(filename: str):
+    reports = read_reports(filename)
+    safety_counter = 0
+
+    for report in reports:
+        is_safe = True
+        is_increasing = None
+        exception_count = 0
+        level_index = 1
+        while level_index < len(report):
+            diff = report[level_index] - report[level_index - 1]
+            
+            if is_increasing is None:
+                # At the first iteration
+                is_increasing = diff > 0
+
+            if exception_count >= 2:
+                is_safe = False
+                break
+
+            elif diff == 0:
+                report.pop(level_index)
+                exception_count += 1
+
+            elif is_increasing and diff < 0:
+                """
+                Remove cur if equal to or less than
+                prev prev
+                """
+                cur = report[level_index]
+                prev_prev = report[level_index - 2]
+                local_diff = cur - prev_prev
+                
+                if cur <= prev_prev and abs(local_diff) <= 3:
+                    report.pop(level_index)
+                else:
+                    report.pop(level_index - 1)
+
+                exception_count += 1
+                level_index -= 1
+            else:
+                if abs(diff) > 3:
+                    if exception_count < 2:
+                        report.pop(level_index)
+                        level_index -= 1
+                        exception_count += 1
+                    else:
+                        is_safe = False
+                        break
+                    
+            level_index += 1
+
+        if is_safe:
+            safety_counter += 1
+            
+    return safety_counter
+    
 if __name__=="__main__":
-    print(verify_report_safety("input2.txt"))
+    print(verify_report_safety_with_exception("test2.txt"))
